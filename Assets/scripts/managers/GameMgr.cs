96b21f030c781c7144ffff7a68fce94038e592f1
Alex <afcarrion85@gmail.com>
2016-11-11 14:32:08 +0100

    create gameMgr

---
+++ Assets/scripts/managers/GameMgr.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMgr : MonoBehaviour {
+
+    private static GameMgr instance = null;
+    private GameObject servers = null;
+
+    public static GameMgr GetInstance() {
+        if (instance == null) {
+            instance = new GameMgr();
+        }
+        return instance;
+    }
+
+    private GameMgr() {
+
+    }
+
+    /***************************************** SERVERS *******************************************/
+
+    protected T AddServer<T>() where T : Component {
+        T t = servers.GetComponent<T>();
+        if (t != null)
+            Component.DestroyImmediate(t);
+        t = servers.AddComponent<T>();
+        return t;
+    }
+
+    /*********************************************************************************************/
+}
--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class player : MonoBehaviour
+public class Player : MonoBehaviour
 {
 
     public int speed;
