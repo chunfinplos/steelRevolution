d8637ef10930a08e1aaa0b40825238496fbd6c14
Alex <afcarrion85@gmail.com>
2016-12-27 12:37:01 +0100

    create spawner and scene managers

---
+++ Assets/scripts/engine/managers/SceneMgr.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneMgr : AComponent {
+
+
+
+    #region MAIN
+
+    protected override void Awake() {
+        base.Awake();
+    }
+
+    protected override void Start() {
+        base.Start();
+    }
+
+    protected override void Update() {
+        base.Update();
+    }
+
+    #endregion MAIN
+}
---
+++ Assets/scripts/engine/managers/SpawnerMgr.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SpawnerMgr {
+    private Dictionary<string, List<GameObject>> m_cache = new Dictionary<string, List<GameObject>>();
+    //private SceneMgr m_sceneMgr;
+    private static int m_staticIDs = 0;
+}
