0a4942bc80d00ea2bac9b976e927ff0a394a8a87
Alex <afcarrion85@gmail.com>
2016-12-28 14:45:53 +0100

    shooting left cannon

--- Assets/scripts/PlayerShoot.cs
+++ Assets/scripts/PlayerShoot.cs
@@ -5,6 +5,10 @@ using System.Collections.Generic;
 public class PlayerShoot : AComponent {
 
     private InputMgr inputMgr;
+    public GameObject prefabL;
+    public GameObject prefabR;
+
+    public float bulletSpeed;
 
     #region MAIN
 
@@ -33,18 +37,30 @@ public class PlayerShoot : AComponent {
     }
 
     public void button(int mCode, Dictionary<inputEvt, bool> buttonData) {
-        if(mCode == inputMgr.inputCtrl.buttons.LEFTSHOOT) {
+        Vector3 targetPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if(mCode == inputMgr.inputCtrl.buttons.LEFTSHOOT && buttonData[inputEvt.DOWN]) {
+            Vector3 sourcePoint = gameObject.transform.GetChild(0).transform.position;
+            Quaternion sourceQ = gameObject.transform.GetChild(0).transform.rotation;
+            sourceQ *= Quaternion.Euler(Vector3.right * 90);
+            GameObject bullet = GameMgr.GetInstance().spawnerMgr.
+                                        CreateNewGameObject(prefabL, sourcePoint, sourceQ);
+
+            //bullet.transform.LookAt(targetPoint);
+            //bullet.GetComponent<Rigidbody>().AddForce(sourcePoint * bulletSpeed);
+            bullet.GetComponent<Rigidbody>().AddRelativeForce(Vector3.up * bulletSpeed);
+
+
+
 
         } else if(mCode == inputMgr.inputCtrl.buttons.RIGHTSHOOT) {
-            Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            
 
-
-            var position = Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
-            position = Camera.main.ScreenToWorldPoint(position);
-            var go = Instantiate(prefab, transform.position, Quaternion.identity) as GameObject;
-            go.transform.LookAt(position);
-            Debug.Log(position);
-            go.rigidbody.AddForce(go.transform.forward * 1000);
+            //var position = Vector3(Input.mousePosition.x, Input.mousePosition.y, distance);
+            //position = 
+            //var go = Instantiate(prefab, transform.position, Quaternion.identity) as GameObject;
+            //go.transform.LookAt(position);
+            //Debug.Log(position);
+            //go.rigidbody.AddForce(go.transform.forward * 1000);
         } else {
 
         }
